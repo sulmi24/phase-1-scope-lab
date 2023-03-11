dac19882ffe7ceaba5d2c1c4e2ce4e198a70e0f2
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 13:45:28 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+var bestCustomer;
+const favoriteCustomer = "GoGo";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  return (bestCustomer = "not bob");
+}
+
+function overwriteBestCustomer() {
+  return (bestCustomer = "maybe bob");
+}
+
+function changeLeastFavoriteCustomer() {
+  return (favoriteCustomer = "superman");
+}
